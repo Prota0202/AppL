c7fb7dc21f5a98c2b4d46287cc7c67f50a80e4b9
AbdelBadi <153845537+Prota0202@users.noreply.github.com>
2025-06-08 17:20:39 +0200

    Ajout des fichiers qui etaient deja fait pour Mobile et re ajustement de la structures des fichiers pour Web et Mobile

---
+++ Mobile/app/(auth)/_Layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { Colors } from '../../src/constants/colors';
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: Colors.background },
+      }}
+    >
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
+  );
+}
