c0badd71dec782bd8daab9ede9d4c18dee00dd09
AbdelBadi <153845537+Prota0202@users.noreply.github.com>
2025-06-09 16:28:30 +0200

    Fichiers inutiles qui peuvent être supprimés

--- Web/src/routes/api/auth/register2.ts
+++
@@ -1,9 +0,0 @@
-import { json } from "@solidjs/router";
-
-export async function GET() {
-  return json({ message: "Register2 GET works!" });
-}
-
-export async function POST() {
-  return json({ message: "Register2 POST works!" });
-}
